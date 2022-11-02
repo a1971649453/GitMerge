4d1396dcf97e953ae621f0393438a03a879dd6b4
a1971649453 <71438393+a1971649453@users.noreply.github.com>
2022-11-02 10:56:01 +0800

    测试分支

---
+++ src/main/java/com/hspedu/hsperp/HspErpApplication.java
@@ -0,0 +1,11 @@
+package com.hspedu.hsperp;
+
+/**
+ * @author 金宗文
+ * @version 1.0
+ */
+public class HspErpApplication {
+    public static void main(String[] args) {
+        System.out.println("HspErpApplication hello");
+    }
+}
