a5caabd71f9f086101ce3b21c4e3d633c6e1d09d
a1971649453 <71438393+a1971649453@users.noreply.github.com>
2022-11-02 11:19:39 +0800

    测试分支

--- src/main/java/com/hspedu/hsperp/HspErpApplication.java
+++ src/main/java/com/hspedu/hsperp/HspErpApplication.java
@@ -6,6 +6,6 @@ package com.hspedu.hsperp;
  */
 public class HspErpApplication {
     public static void main(String[] args) {
-        System.out.println("HspErpApplication hello");
+        System.out.println("HspErpApplication hello 2.0分支测试工作");
     }
 }
